6809c5277fa4db5414b23f70b6837f96f8258811
CiroLee <ciromail@126.com>
2022-05-27 15:21:46 +0800

    feat: 帮助信息优化

--- src/features.ts
+++ src/features.ts
@@ -2,14 +2,17 @@ import pkg from '../package.json';
 import { intersection } from './utils/utils';
 import Log from './log';
 const logger = new Log();
-const strategies = {
+const strategies: {
+  [key: string]: () => void;
+} = {
   version: () => {
     logger.log(pkg.version);
   },
   help: () => {
     const helps = `Options:
+  init                 初始化项目
   commit               使用commitlint规范提交信息
-  deploy [-u]          将当前分支推送到远程仓库。如果是新仓库, 请使用 fe/fe-cli deploy -u
+  deploy [-u]          将当前分支推送到远程仓库。如果是新仓库, 请使用 mew/mew-cli deploy -u
   page                 创建页面(自动同步到路由配置)
   -v, --version        输出本版号
   -h, --help           输出帮助信息
@@ -35,5 +38,5 @@ const getStragegyName = (optionsArgs: object) => {
 
 export const executeFeature = (feature: object) => {
   const strategy = getStragegyName(feature);
-  return strategy && (strategies as any)[strategy]();
+  return strategy && strategies[strategy]();
 };
